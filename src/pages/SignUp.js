3fb5760150044ea2a8e19c3005f4709269df10f5
Rashita <rrashita_be19@thapar.edu>
2022-04-28 11:25:21 +0530

    fix: upload image added

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,13 +1,13 @@
 export const SignUp = () => {
     return(
         <div>
-            <div className="bg-cyan-400 w-5/12 m-auto py-10">
+            <div className="bg-cyan-400 w-5/12 m-auto py-10 flex flex-col items-start pl-48">
             
                 <input className="mb-6 p-1" placeholder="Name"></input><br/>
                 <input className="mb-6 p-1" placeholder="E-mail ID"></input><br/>
                 <input className="mb-6 p-1" placeholder="Enter Password"></input><br/>
                 <input className="mb-6 p-1" placeholder="Confirm Password"></input><br/>
-                <input className="mb-6 p-1" placeholder="Upload Photo"></input><br/>
+                <input className="mb-6 p-1" placeholder="Upload Photo" type="file" id="img" name="img" accept="image/*"></input><br/>
                 <button className="bg-slate-600 px-16 py-1 text-white mb-6" >Sign In</button><br/>
             </div>
         </div>
