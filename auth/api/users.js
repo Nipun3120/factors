a5874f645335b34058408eb009547895dfd476a3
Nipun Verma <Nipunverma39@gmail.com>
2022-04-28 09:34:15 +0530

    wip

--- auth/api/users.js
+++ auth/api/users.js
@@ -1,6 +1,30 @@
 const bcrypt = require("bcrypt");
 const express = require("express");
+const multer = require('multer');
+
 const router = express.Router();
 const User = require("../database/users");
+
+
+//define storage for the images
+const storage = multer.diskStorage({
+     //destination for files
+     destination: function (request, file, callback) {
+       callback(null, './public/uploads/images');
+     },
+   
+     //add back the extension
+     filename: function (request, file, callback) {
+       callback(null, Date.now() + file.originalname);
+     },
+   });
+   
+   //upload parameters for multer
+   const upload = multer({
+     storage: storage,
+     limits: {
+       fieldSize: 1024 * 1024 * 3,
+     },
+   });
 
 module.exports = router;
--- auth/database/users.js
+++ auth/database/users.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
-})
+  name: { type: String, default: null },
+  email: { type: String, unique: true },
+  password: { type: String },
+  image: { data: Buffer, contentType: String },
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
